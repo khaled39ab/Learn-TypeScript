81c9afe7252bfc7260d8d6e6b3a54ec35a08172f
khaled39ab <khaled39ab@gmail.com>
2022-12-29 23:59:08 +0600

    second program data type

---
+++ built-in-type/index.ts
@@ -0,0 +1,11 @@
+//built-in data type: number, string, boolean, void, undefined, null
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let isActive: boolean;
+
+userId = 101;
+firstName = "Japan";
+lastName = "Dactar";
+isActive = true;
