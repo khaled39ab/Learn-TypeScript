a78e2d6f13e005fcc56ef3f4ad2748d93fea8970
khaled39ab <khaled39ab@gmail.com>
2022-12-29 23:45:00 +0600

    first typescript program

--- first-program/index.js
+++ first-program/index.js
@@ -1,3 +1,7 @@
 var user = 'World';
 var message = "Hello";
 console.log(message, user);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(20, 40));
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,3 +1,9 @@
 let user = 'World';
 let message = "Hello"
 console.log(message, user);
+
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
+
+console.log(add(20, 40));
