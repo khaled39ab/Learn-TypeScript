3c5aa0d84a1dce6325952a239b011b9fedd1fe0d
Khaled39ab <khaled39ab@gmail.com>
2023-01-04 23:22:17 +0600

    learn ts union array type

--- array -data-type/index.js
+++ array -data-type/index.js
@@ -4,3 +4,6 @@ console.log(userList.sort());
 var userList2; //another syntax
 userList2 = ['Josim', 'Nijam', 'Joy'];
 console.log(userList2.length);
+var multipleType;
+multipleType = ['Kabir', 30, 21, "Azim"];
+console.log(multipleType[3]);
--- array -data-type/index.ts
+++ array -data-type/index.ts
@@ -1,11 +1,19 @@
 let userList: string[];
-
 userList = ["Anis", "Alom", "Jalal", "Delo"];
 
 console.log(userList.sort())
 
+
+
 let userList2: Array<string>;  //another syntax
 userList2= ['Josim', 'Nijam', 'Joy'];
 
-console.log(userList2.length)
+console.log(userList2.length);
+
+
+
+let multipleType: (number | string) [];
+multipleType= ['Kabir', 30, 21, "Azim"];
+
+console.log(multipleType[3])
 
