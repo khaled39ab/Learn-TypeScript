f1f038c89ffa459d409937c470b8df0b338b8b1c
Khaled39ab <khaled39ab@gmail.com>
2023-01-04 23:17:39 +0600

    learn ts array type

---
+++ array -data-type/index.js
@@ -0,0 +1,6 @@
+var userList;
+userList = ["Anis", "Alom", "Jalal", "Delo"];
+console.log(userList.sort());
+var userList2; //another syntax
+userList2 = ['Josim', 'Nijam', 'Joy'];
+console.log(userList2.length);
---
+++ array -data-type/index.ts
@@ -0,0 +1,11 @@
+let userList: string[];
+
+userList = ["Anis", "Alom", "Jalal", "Delo"];
+
+console.log(userList.sort())
+
+let userList2: Array<string>;  //another syntax
+userList2= ['Josim', 'Nijam', 'Joy'];
+
+console.log(userList2.length)
+
