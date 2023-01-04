000b8761e8cfcffdf9beea7990727ccb80e6324f
Khaled39ab <khaled39ab@gmail.com>
2023-01-04 23:37:39 +0600

    learn ts tuple type

---
+++ tuple-type/index.js
@@ -0,0 +1,5 @@
+// key value pair type array
+var users;
+users = [101, 'Limon'];
+users.unshift(102, 'Kalu');
+console.log(users);
---
+++ tuple-type/index.ts
@@ -0,0 +1,8 @@
+// key value pair type array
+
+let users: [number, string];
+users = [101, 'Limon']
+
+users.unshift(102, 'Kalu');
+
+console.log(users)
