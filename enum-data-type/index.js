d2835c59c094bc188d0dd0af965944d885614c60
Khaled39ab <khaled39ab@gmail.com>
2023-01-05 22:43:37 +0600

    learn ts enum numeric type

--- enum-data-type/index.js
+++ enum-data-type/index.js
@@ -1,6 +1,15 @@
+// enum -> help to store constants in typescript and no duplicate data
+/*
+enum are 3 type.
+1- numeric enum
+2- string enum
+3- Heterogeneous enum
+*/
+//numeric enum 
 var requestType;
 (function (requestType) {
-    requestType[requestType["readData"] = 0] = "readData";
-    requestType[requestType["getData"] = 1] = "getData";
+    requestType[requestType["readData"] = 4] = "readData";
+    requestType[requestType["getData"] = 5] = "getData";
+    requestType[requestType["deleteData"] = 6] = "deleteData";
 })(requestType || (requestType = {}));
 console.log(requestType);
--- enum-data-type/index.ts
+++ enum-data-type/index.ts
@@ -1,6 +1,19 @@
+// enum -> help to store constants in typescript and no duplicate data
+
+/*  
+enum are 3 type. 
+1- numeric enum 
+2- string enum
+3- Heterogeneous enum
+*/
+
+
+//numeric enum 
+
 enum requestType {
-    readData,
-    getData
+    readData = 4,
+    getData,
+    deleteData
 }
 
 console.log(requestType)
