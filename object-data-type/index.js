1fa404c9872c2e3902f05492c788e0eca6b582d4
Khaled39ab <khaled39ab@gmail.com>
2023-01-06 23:25:52 +0600

    learn ts object data type

--- object-data-type/index.js
+++ object-data-type/index.js
@@ -1,11 +1,12 @@
-// object Type: can store value as key value pair
-// let user: object;
-// user = { name: 'amin' };
-var users;
+// ---------object Type: can store value as key value pair-----------
+var user;
+user = { name: 'amin' };
+// let users: object[];
 // users = [
 //     {name: 'Alom', id: 103},
 //     {name: 'Atik', id: 104},
 // ]
+var users;
 users = [];
 var user1;
 user1 = { name: 'Alom', id: 102 };
@@ -15,3 +16,6 @@ var user2;
 user2 = { name: 'Salim', id: 101 };
 users.push(user2);
 console.log(users);
+for (var key in users) {
+    console.log(users[key]['name']);
+}
--- object-data-type/index.ts
+++ object-data-type/index.ts
@@ -1,14 +1,15 @@
-// object Type: can store value as key value pair
+// ---------object Type: can store value as key value pair-----------
 
-// let user: object;
-// user = { name: 'amin' };
+let user: object;
+user = { name: 'amin' };
 
-let users: object[];
+// let users: object[];
 // users = [
 //     {name: 'Alom', id: 103},
 //     {name: 'Atik', id: 104},
 // ]
 
+let users: object[];
 users = [];
 
 let user1: { name: string, id: number }
@@ -17,8 +18,12 @@ user1 = { name: 'Alom', id: 102 }
 users.push(user1)
 console.log(users)
 
-let user2: {name: string, id: number}
-user2 = { name: 'Salim', id: 101};
+let user2: { name: string, id: number }
+user2 = { name: 'Salim', id: 101 };
 
-users.push(user2)
-console.log(users)
+users.push(user2);
+console.log(users);
+
+for (const key in users) {
+    console.log(users[key]['name'])
+}
