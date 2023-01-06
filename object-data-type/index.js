e8e1fc92664a9eeaafae3ee5806593352545177b
Khaled39ab <khaled39ab@gmail.com>
2023-01-06 23:10:04 +0600

    learn ts object type

---
+++ object-data-type/index.js
@@ -0,0 +1,8 @@
+var user;
+user = { name: 'amin' };
+var users;
+users = [
+    { name: 'Alom', id: 103 },
+    { name: 'Atik', id: 104 },
+];
+console.log(users);
---
+++ object-data-type/index.ts
@@ -0,0 +1,10 @@
+let user: object;
+user = { name: 'amin' };
+
+let users: object[];
+users = [
+    {name: 'Alom', id: 103},
+    {name: 'Atik', id: 104},
+]
+
+console.log(users)
