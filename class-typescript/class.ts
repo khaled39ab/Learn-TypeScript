fb3ece7a6054de3ad19a531a37bd41cfcec8fb87
khaled39ab <khaled39ab@gamil.com>
2023-01-09 23:56:27 +0600

    new file

--- class-typescript/class.ts
+++ class-typescript/class.ts
@@ -2,6 +2,7 @@
 
 // In TypeScript, the class keyword provides a more familiar syntax for generating constructor functions and performing simple inheritance. 
 
+
 class User {
     // properties, methods, constructor
     userName: string;
@@ -22,6 +23,3 @@ class User {
   
   let user2 = new User("Rakibul Islam", 31);
   user2.display();
-
-  let user3 = new User("Hasib", 12);
-  user3.display();
