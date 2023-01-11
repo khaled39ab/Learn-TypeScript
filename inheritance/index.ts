2a595843faac75beaebcb4628d2ef5476bcdf270
khaled39ab <khaled39ab@gmail.com>
2023-01-11 23:32:06 +0600

    typescript inherite user

--- inheritance/index.ts
+++ inheritance/index.ts
@@ -2,19 +2,33 @@ class User {
     // properties, methods, constructor
     userName: string;
     age: number;
-  
+
     constructor(userName: string, age: number) {
-      this.userName = userName;
-      this.age = age;
+        this.userName = userName;
+        this.age = age;
     }
-  
+
     display(): void {
-      console.log(`username: ${this.userName}, age: ${this.age}`);
+        console.log(`username: ${this.userName}, age: ${this.age}`);
     }
-  }
-  
-  let user1 = new User("Saiful Islam", 25);
-  user1.display();
-  
-  let user2 = new User("Rakibul Islam", 31);
-  user2.display();
+}
+
+class Student extends User {
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number) {
+        super(userName, age);
+        this.studentId = studentId;
+    }
+    display(): void {
+        console.log(
+            `username: ${this.userName}, age: ${this.age}, id: ${this.studentId}`
+        );
+    }
+}
+
+let user1 = new User("Saiful Islam", 25);
+user1.display();
+
+let user2 = new User("Rakibul Islam", 31);
+user2.display();
