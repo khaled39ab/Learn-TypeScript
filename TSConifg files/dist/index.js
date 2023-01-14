442704bc76c1519408624c113b93b7b87b76bca4
khaled39ab <khaled39ab@gmail.com>
2023-01-14 23:52:15 +0600

    added more config

--- TSConifg files/dist/index.js
+++ TSConifg files/dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("Hello, I'm from TypeScript");
+const display = (message) => {
+    let sum = 20; // for use noUnusedLocals config, it's getting error
+    console.log(message + ", " + "I'm from function");
+    //if parameter is not use it'll be error for use noUnusedLocals config,
+};
+display("Hi");
--- TSConifg files/src/index.ts
+++ TSConifg files/src/index.ts
@@ -1,1 +1,9 @@
 console.log("Hello, I'm from TypeScript");
+
+const display = (message: string) =>{
+    let sum: number = 20; // for use noUnusedLocals config, it's getting error
+    console.log(message + ", "+"I'm from function");
+    //if parameter is not use it'll be error for use noUnusedLocals config,
+}
+
+display("Hi");
