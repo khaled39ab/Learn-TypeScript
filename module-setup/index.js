eacb7c55a2b8436f144bb0c5432e109016165ac0
khaled39ab <khaled39ab@gmail.com>
2023-01-14 12:03:48 +0600

    normal code setup

---
+++ module-setup/index.js
@@ -0,0 +1,7 @@
+var studentId = 12123;
+var studentName = 'Abir';
+var studentAge = 23;
+function studentInfo() {
+    console.log("Id: ".concat(studentId, ", Name: ").concat(studentName, ", Age: ").concat(studentAge));
+}
+studentInfo();
---
+++ module-setup/index.ts
@@ -0,0 +1,9 @@
+let studentId: number = 12123;
+let studentName: string = 'Abir';
+let studentAge: number = 23;
+
+function studentInfo(): void{
+    console.log(`Id: ${studentId}, Name: ${studentName}, Age: ${studentAge}`);
+}
+
+studentInfo()
